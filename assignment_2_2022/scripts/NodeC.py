5643d1733bfb017eb9ba9406b4cec846e8bfe1b3
Manuel <97695681+manudelu@users.noreply.github.com>
2023-03-12 15:52:15 +0100

    Add files via upload

---
+++ assignment_2_2022/scripts/NodeC.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+import rospy
+import math
+
+from assignment_2_2022.msg import RobotMsg
+
+def callback_subscriber(data):
+	# Get the desired position from the ROS parameter server
+	des_pos_x = rospy.get_param("des_pos_x")
+	des_pos_y = rospy.get_param("des_pos_y")
+
+	# Calculate the distance between the current and the desired position
+	distance = math.sqrt(pow(des_pos_x - data.x, 2) + (pow(des_pos_y - data.y, 2))
+	
+	# Calculate the velocity   
+	vel = math.sqrt(pow(data.vel_x, 2) + pow(data.vel_y, 2))
+    
+	# Print distance and velocity
+	rospy.loginfo("Distance to the goal: ", distance)
+	rospy.loginfo("Average speed: ", vel)
+    
+
+if __name__ == '__main__':
+
+	# Init the Node
+	rospy.init_node("NodeC")
+	
+	# Subscribe to the RobotMsg topic and pass the info to the callback function
+	rospy.Subscriber('my_pos_and_my_vel', RobotMsg, callback_subscriber)
+	
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		rate.sleep()
+	
+	# Keep the node running
+	rospy.spin()
+	
