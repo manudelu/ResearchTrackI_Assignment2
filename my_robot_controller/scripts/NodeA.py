b49427e857c84f8653aec8615782ab2bb2526147
Manuel <97695681+manudelu@users.noreply.github.com>
2023-03-12 01:44:00 +0100

    Add files via upload

--- my_robot_controller/scripts/NodeA.py
+++ my_robot_controller/scripts/NodeA.py
@@ -25,11 +25,11 @@ def callback(msg):
 	
 	# Set the robot position and linear velocity
 	my_custom_msg.x = msg.pose.pose.position.x
-    my_custom_msg.y = msg.pose.pose.position.y
-    my_custom_msg.vel_x = msg.twist.twist.linear.x
-    my_custom_msg.vel_y = msg.twist.twist.linear.y
+	my_custom_msg.y = msg.pose.pose.position.y
+	my_custom_msg.vel_x = msg.twist.twist.linear.x
+	my_custom_msg.vel_y = msg.twist.twist.linear.y
     
-    # Publish the message
+	# Publish the message
 	if not rospy.is_shutdown():
 		my_pub.publish(my_custom_msg)
 
@@ -48,17 +48,17 @@ def set_target():
 	# Print the selected coordinates 
 	print("The position of the target is: (", x_pos, ", ", y_pos, ")")
 
-    # Creates a goal message with the target coordinates
-    goal = assignment_2_2022.msg.PlanningGoal()
-    goal.target_pose.pose.position.x = x_pos
-    goal.target_pose.pose.position.y = y_pos
+	# Creates a goal message with the target coordinates
+	goal = assignment_2_2022.msg.PlanningGoal()
+	goal.target_pose.pose.position.x = x_pos
+	goal.target_pose.pose.position.y = y_pos
     
-    # Send the goal to the action server
-    client.send_goal(goal)
-    print("The target has been successfully sent to the sever!!")
+	# Send the goal to the action server
+	client.send_goal(goal)
+	print("The target has been successfully sent to the sever!!")
  
-    # Back to the interface function
-    user_interface()	
+	# Back to the interface function
+	user_interface()	
 
 def cancel_target():
 	"""
@@ -72,12 +72,12 @@ def cancel_target():
 	if client.get_state() == actionlib.GoalStatus.ACTIVE:
     	client.cancel_goal()
     	print("The target has been cancelled successfully!!")
-    # If there is no active goal then display an error message
-    else:
-    	print("Error. There's no target to cancel.")
+	# If there is no active goal then display an error message
+	else:
+		print("Error. There's no target to cancel.")
         
-    # Back to the interface function
-    user_interface()
+	# Back to the interface function
+	user_interface()
 
 def user_interface():
 	"""
@@ -89,36 +89,36 @@ def user_interface():
 	Args: None
 	"""
 	# Create a new client
-    client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2022.msg.PlanningAction)	
+	client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2022.msg.PlanningAction)	
     
-    # Wait for the server to be ready to receive the goal 
-    client.wait_for_server()
+	# Wait for the server to be ready to receive the goal 
+	client.wait_for_server()
 	
 	# Clean the screen
 	rospy.sleep(2) 
 	os.system('clear')
 	
 	print("###############################################\n")    
-    print("##          Robot Control Interface          ##\n")
-    print("###############################################\n")
-    print("## -------> Select 1: Target Position        ##\n")
-    print("## -------> Select 2: Cancel                 ##\n")
-    print("## -------> Select 3: Exit                   ##\n")
-    print("###############################################\n") 
+	print("##          Robot Control Interface          ##\n")
+	print("###############################################\n")
+	print("## -------> Select 1: Target Position        ##\n")
+	print("## -------> Select 2: Cancel                 ##\n")
+	print("## -------> Select 3: Exit                   ##\n")
+	print("###############################################\n") 
         
-    # Ask the user to select an operation
-    choice = input("Select your operation: ")
+	# Ask the user to select an operation
+	choice = input("Select your operation: ")
         
-    # Check the user selection
-    if   (choice == "1"):
-   		set_target()
-    elif (choice == "2"):
-        cancel_target()     	
-    elif (choice == "3"):
-       	rospy.signal_shutdown("Exiting...")    
-    else:
-        print("Wrong choise!! Try Again...")
-        user_interface()      
+	# Check the user selection
+	if   (choice == "1"):
+		set_target()
+	elif (choice == "2"):
+		cancel_target()     	
+	elif (choice == "3"):
+		rospy.signal_shutdown("Exiting...")    
+	else:
+		print("Wrong choise!! Try Again...")
+		user_interface()      
 	
 if __name__ == '__main__':
 
@@ -129,8 +129,8 @@ if __name__ == '__main__':
 	global pub
 	pub = rospy.Publisher("/pos_vel", RobotMsg, queue_size = 1)
 
-    # Define a subscriber which listen to the Odometry message and calls the callback function
-    rospy.Subscriber("/odom", Odometry, callback)
+	# Define a subscriber which listen to the Odometry message and calls the callback function
+	rospy.Subscriber("/odom", Odometry, callback)
     
 	# Call the UI function
 	user_interface()
